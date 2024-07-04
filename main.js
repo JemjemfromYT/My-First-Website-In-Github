d2ad62eaf3a15ea2c2fe6f20948412179ac3ea6c
JemjemfromYT <saavedrajames10@gmail.com>
2024-07-04 18:11:14 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-alert("Hello, this is an alert message!");
-
-function Alert() {
-    alert("it's James Virus");
-}
-
-function Virus() {
-    setInterval(Alert, 1000); // Change the interval to 1000 milliseconds (1 second)
-}
-
-Virus();
-
+// Add all your JS here
